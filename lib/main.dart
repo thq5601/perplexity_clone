6b2aa87ab9daed4537bdb690dbad90f948df54ae
QuanTH <thq5601@gmail.com>
2025-05-15 11:12:24 +0700

    Refactor HomePage widget and update import paths; add AppColors class for theme management

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:perflexity/home_page.dart';
+import 'package:perflexity/presentation/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,12 +8,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const HomePage(),
